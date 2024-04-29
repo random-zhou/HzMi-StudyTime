e3bfef08b246a95a1e79adf5a30123880110543b
“random-zhou” <2501601106@qq.com>
2024-04-29 09:23:05 +0800

    v20240429

--- usual_control_algorithm/filters.h
+++ usual_control_algorithm/filters.h
@@ -1,11 +1,3 @@
-/*
- * @Author: “random-zhou” 2501601106@qq.com
- * @Date: 2024-04-18 00:23:06
- * @LastEditors: “random-zhou” 2501601106@qq.com
- * @LastEditTime: 2024-04-29 16:34:11
- * @FilePath: \HzMi-StudyTime\usual_control_algorithm\filters.h
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 #ifndef __FILTERS_H__
 #define __FILTERS_H__
 
